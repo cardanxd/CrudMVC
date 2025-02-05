33e7e88f7fd4f065888d224305b6315bd688d1f9
Cardanxd <carloslopezp1497@gmail.com>
2025-02-05 17:46:18 -0600

    crud finalizado

--- Controllers/EmpleadoController.cs
+++ Controllers/EmpleadoController.cs
@@ -1,4 +1,5 @@
 using CrudMVC.Context;
+using CrudMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,5 +19,50 @@ namespace CrudMVC.Controllers
             return View(lista);
         }
 
+        [HttpGet]
+        public IActionResult Nuevo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Nuevo(Empleado empleado)
+        {
+            if (ModelState.IsValid)
+            {
+                await _context.Empleados.AddAsync(empleado);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Lista));
+            }
+            return View(empleado);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Editar(int id)
+        {
+            var empleado = await _context.Empleados.FindAsync(id);
+            return View(empleado);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Editar(Empleado empleado)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Empleados.Update(empleado);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Lista));
+            }
+            return View(empleado);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Eliminar(int id)
+        {
+            Empleado empleado = await _context.Empleados.FirstAsync(x => x.IdEmpleado == id);
+            _context.Empleados.Remove(empleado);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Lista));
+        }
     }
 }
